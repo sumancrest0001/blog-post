71eef27357756135f971116c0cec160f8c22a25b
sumancrest0001 <suman.crest0001@gmail.com>
2019-12-16 17:55:32 +0545

    completed till pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,14 @@ class UsersController < ApplicationController
        render 'edit'
     end
   end
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
 
   def show
       @user = User.find(params[:id])
+      @user_posts = @user.posts.paginate(page: params[:page], per_page: 5)
   end
 
   def destroy
